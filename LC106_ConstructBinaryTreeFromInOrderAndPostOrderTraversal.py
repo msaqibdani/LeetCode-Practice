3f9c568da679c2c93a0c047963bad95f3575f3ad
Daniyal Saqib <daniyal@formotiv.com>
2020-08-20 21:18:19 -0400

    LC 106 Construct Binary Tree from InOrder and Post Order Traversal

---
+++ LC106_ConstructBinaryTreeFromInOrderAndPostOrderTraversal.py
@@ -0,0 +1,33 @@
+class TreeNode():
+  def __init__(self, val):
+    self.val = val
+    self.right = None
+    self.left = None
+  
+
+def buildTree(inorder, postorder):
+  
+  def helper(left, right):
+    
+    if left > right:
+      return None
+      
+    
+    #popping the last element
+    root = postorder.pop()
+    #creating the node
+    treeRoot = TreeNode(root)
+    #index value in inorder
+    index_value = indexes[root]
+    
+    
+    treeRoot.right = helper(index_value+1, right)
+    treeRoot.left = helper(left, index_value-1)
+    
+    return treeRoot
+
+
+
+  
+  indexes = {key:value for value, key in enumerate(inorder)}
+  return helper(0, len(inorder) - 1)
